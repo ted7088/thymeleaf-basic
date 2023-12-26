11488f5381e4319b79977f7ab3c24772d95dab72
master <ted3131@diquest.com>
2023-12-26 15:50:56 +0900

    commit

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -108,6 +108,13 @@ public class BasicController {
     public String attribute() {
         return "basic/attribute";
     }
+
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+
+        return "basic/condition";
+    }
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
@@ -115,6 +122,7 @@ public class BasicController {
         list.add(new User("userC", 30));
         model.addAttribute("users", list);
     }
+
 
     @Data
     static class User {

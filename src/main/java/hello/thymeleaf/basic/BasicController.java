983beb25d4d14047024ee30a6a30593733fbbd34
master <ted3131@diquest.com>
2023-12-27 10:40:42 +0900

    commit

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -137,6 +137,10 @@ public class BasicController {
 
         return "basic/javascript";
     }
+
+
+
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
